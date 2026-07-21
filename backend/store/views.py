c738d14deff5d4825e52367bbb3241ac1d89d440
Abhishek Tiwari <abhishektiwari02000@gmail.com>
2026-07-21 15:37:23 +0530

    linked frontend with backend and Database

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def home(request):
+    data = {
+        "message": "Welcome to the E-Commerce API",
+    }
+    return JsonResponse(data)
