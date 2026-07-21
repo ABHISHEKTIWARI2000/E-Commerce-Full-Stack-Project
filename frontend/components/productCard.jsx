d518ea8ab93ff6384bfffddfa1532a7b807d5eaa
Abhishek Tiwari <abhishektiwari02000@gmail.com>
2026-07-21 20:04:50 +0530

    image integrated

--- frontend/components/productCard.jsx
+++ frontend/components/productCard.jsx
@@ -1,14 +1,21 @@
 function ProductCard({ product }) {
+    const BASEURL = import.meta.env.VITE_DJANGO_BASE_URL;
     return (
-        <div className="bg-white p-4 rounded shadow hover:shadow-lg transition duration-300">
-            <img
-                src={product.image}
-                alt={product.name}
-                className="w-full h-48 object-cover mb-4 rounded-lg"
-            />
+        <div className="bg-white p-4 rounded shadow hover:shadow-lg hover:scale-101 transition duration-300">
+            {product.image ? (
+                <img
+                    src={`${BASEURL}${product.image}`}
+                    alt={product.name}
+                    className="w-full h-48 object-cover mb-4 rounded-lg"
+                />
+            ) : (
+                <div className="flex h-48 items-center justify-center rounded-lg bg-gray-200 text-gray-500">
+                    No image available
+                </div>
+            )}
             <h2 className="text-lg font-semibold text-gray-800 truncate">{product.name}</h2>
-            <p className="text-gray-600 font-medium">{product.price}</p>
-            <p className="text-lg font-bold">${product.description}</p>
+            <p className="text-gray-600 font-medium">${product.price}</p>
+            <p className="text-lg font-bold">{product.description}</p>
         </div>
     );
 }
